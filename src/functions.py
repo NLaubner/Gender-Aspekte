b8ca40b1eaa1a9458a32bcdbb82d211bdd651ef2
Nico Laubner <laubnerni98765@th-nuernberg.de>
2026-07-07 12:25:56 +0200

    schöne Diagramme

--- src/functions.py
+++ src/functions.py
@@ -9,6 +9,7 @@ from collections import Counter, defaultdict
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from scipy.stats import spearmanr, chi2_contingency
+import textwrap
 
 import re
 import spacy
@@ -200,7 +201,7 @@ def plot_gender(data):
 
 # Plot zur Pointwise Mutual Information (PMI)
 def plot_pmi(pmi_data: dict, top_n: int = 20):
-    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    fig, axes = plt.subplots(1, 2, figsize=(13, 5.5))
     fig.patch.set_facecolor("#fcfbf9")
 
     for ax, gender in zip(axes, ("männlich", "weiblich")):
@@ -220,15 +221,18 @@ def plot_pmi(pmi_data: dict, top_n: int = 20):
         ax.tick_params(axis="y", colors="#0C447C", labelsize=12)
         ax.grid(axis="x", color="#B5D4F4", linestyle="--", linewidth=0.6, alpha=0.7)
         ax.set_xlabel("PMI (Bits)")
-        ax.set_title(f"Top-Wörter assoziiert mit {'männlichen' if gender == 'männlich' else 'weiblichen'} Wissenschaftler:innen", fontsize=20, pad = 15)
+        titel = f"Top-Wörter assoziiert mit {'männlichen' if gender == 'männlich' else 'weiblichen'} Wissenschaftler:innen"
+        titel = "\n".join(textwrap.wrap(titel, width=26))
+        ax.set_title(titel, fontsize=14, pad=15)
         ax.axvline(0, color="#185FA5", linewidth=0.9, linestyle="-")
     x_min = min(ax.get_xlim()[0] for ax in axes)
     x_max = max(ax.get_xlim()[1] for ax in axes)
     for ax in axes:
         ax.set_xlim(x_min, x_max)
-
+    
+    fig.subplots_adjust(wspace=0.45)
     plt.suptitle("Wortassoziationen nach Geschlecht (PMI)",
-                 fontsize=20, pad=15, color="#042C53", y=1.01)
+                 fontsize=20, color="#042C53", y=1.01)
     plt.tight_layout()
     plt.savefig("../figures/pmi.png", dpi=300)
     plt.show()
@@ -250,11 +254,20 @@ def plot_rank_scatter(freq_m: dict, freq_f: dict):
     interesting = sorted(common, key=lambda w: abs(freq_m[w] - freq_f[w]),
                          reverse=True)[:12]
 
+    MANUELLE_OFFSETS = {
+    "studieren": (30, 18),  
+    }
     texts = []
     for w in interesting:
         x, y = np.log10(freq_m[w]), np.log10(freq_f[w])
-        texts.append(ax.text(x, y, w, fontsize=11, color="#042C53", alpha=0.9))
-
+        if w in MANUELLE_OFFSETS:
+            dx, dy = MANUELLE_OFFSETS[w]
+            ax.annotate(w, xy=(x, y), xytext=(dx, dy), textcoords="offset points",
+                    fontsize=11, color="#042C53", alpha=0.9,
+                    arrowprops=dict(arrowstyle="-", color="#378ADD", lw=0.7))
+        else:
+            texts.append(ax.text(x, y, w, fontsize=11, color="#042C53", alpha=0.9))
+    
     adjust_text(
         texts,
         ax=ax,
